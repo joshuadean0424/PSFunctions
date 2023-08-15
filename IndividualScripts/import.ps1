9885bca75993122f89a0a722d1d190cc569f662b
Joshua Dean <joshuadean0424@gmail.com>
2023-08-15 14:28:48 -0500

    Update import.ps1
    add import psfunctions to import script

--- IndividualScripts/import.ps1
+++ IndividualScripts/import.ps1
@@ -1,5 +1,6 @@
 ﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
+Import-Module .\PSFunctions
 Import-Module psTrustedHosts -Force
 Import-Module PoshWSUS -Force
 Import-Module PSWindowsUpdate -force

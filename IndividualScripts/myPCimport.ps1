5b15bcc649bf6cffcfd04661ccf3fe24cec83f87
Joshua Dean <joshuadean0424@gmail.com>
2023-08-15 16:35:35 -0500

    Add Server file and rename script
    Added a server import script to import current module packages being used locally and rename personal pc script that imports all modules and starts a transcript

---
+++ IndividualScripts/myPCimport.ps1
@@ -0,0 +1,12 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+Import-Module PSFunctions -Verbose -Force
+Import-Module psTrustedHosts -Force
+Import-Module PoshWSUS -Force
+Import-Module PSWindowsUpdate -force
+Import-Module PowerShellGet -force
+
+
+Start-Transcript "D:\Powershell\transcript\transcript0.txt" -Append
+cd C:\
+cls

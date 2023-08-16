e3f4978480f260376f445d92237f0ae8b537fe96
Joshua Dean <joshuadean0424@gmail.com>
2023-08-16 13:12:25 -0500

    Update ServerImport.ps1

--- IndividualScripts/ServerImport.ps1
+++ IndividualScripts/ServerImport.ps1
@@ -1,30 +1,24 @@
-﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+﻿##Steps to use to setup Powershell Modules
+## Joshua Dean
+## Mobis AL
 
-##---------------------------
-##Function Install-MyModules
-##This imports all modules from Local PowerShell Repository
-##Author: Joshua Dean
-##Company: Mobis AL
-##---------------------------
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+$remotePath = '\\10.37.202.199\Software\LocalPowerShellRepo'
+$localPsRepoParam = @{
+	Name = 'MALLocalRepo'
+	SourceLocation = $remotePath
+	PublishLocation = $remotePath
+	InstallationPolicy = 'Trusted'
+} 
+Register-PSRepository @localPsRepoParam
 
+Set-ExecutionPolicy Bypass
+Import-Module PowerShellGet
 Get-PSRepository
-function Install-MyModules {
-    Install-Module PSFunctions -Verbose -Force
-    Install-Module psTrustedHosts -Force
-    Install-Module PoshWSUS -Force
-    Install-Module PSWindowsUpdate -force
-    Install-Module PowerShellGet -force    
-} 
-##---------------------------
-##Function Import-MyModules
-##This imports all modules from Local PowerShell Repository
-##Author: Joshua Dean
-##Company: Mobis AL
-##---------------------------
-function Import-MyModules {
-    Import-Module PSFunctions -Verbose -Force
-    Import-Module psTrustedHosts -Force
-    Import-Module PoshWSUS -Force
-    Import-Module PSWindowsUpdate -force
-    Import-Module PowerShellGet -force    
-} 
+Install-Module -Name PSFunctions -RequiredVersion "4.0"
+
+Install-MyModules
+Import-MyModules
+Set-Default
+Clear-Host
+Write-Host "All Modules Installed and Imported."
